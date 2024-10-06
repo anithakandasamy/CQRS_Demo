70df82b5698a9139d27e7edd506664f148bf77aa
anithakandasamy <anitha.kandasamy@shell.com>
2024-10-06 06:03:06 +0530

    Add project files.

---
+++ DataAccess/DemoAppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CQRS_Implementation.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_Implementation.DataAccess
+{
+    public sealed class DemoAppDbContext: DbContext
+    {
+        public DemoAppDbContext(DbContextOptions<DemoAppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(p => p.Id);
+            modelBuilder.Entity<User>().HasData(
+                new User(1,"Lithisha", "lithisha@xxx.com","INAKQ2"),
+                new User(2, "Keerthi Vasan", "keerthivasan@xxx.com", "KVRKS5")
+                );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("DemoAppDB");
+        }
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,24 @@
+﻿namespace CQRS_Implementation.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailId { get; set; }
+        public string ShortId { get; set; }
+
+        //Parameterless constructor for EF core
+        private User()
+        {
+        }
+
+        public User(int id, string name, string emailId, string shortId)
+        {
+            Id  = id;
+            Name = name;
+            EmailId = emailId;
+            ShortId = shortId;
+        }
+
+    }
+}
